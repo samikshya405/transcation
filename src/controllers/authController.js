6aa052e34e29d2db5ed10fb52a1a6d9e2fedd97f
samikshya <samikshyakharel405@gmail.com>
2026-08-17 19:28:09 +0930

    add user authentication with bcrypt and JWT

---
+++ src/controllers/authController.js
@@ -0,0 +1,55 @@
+import pool from "../config/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const registeruser = async (req, res) => {
+  const userDetail = req.body;
+  const hashedPassword = await bcrypt.hash(userDetail.password, 10);
+  const result = await pool.query(
+    `INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING * `,
+    [userDetail.name, userDetail.email, hashedPassword],
+  );
+  res.status(201).json({
+    status: "success",
+    message: "user created successfully",
+    users: result.rows[0],
+  });
+};
+const loginUsers = async (req, res) => {
+  const userDetails = req.body;
+  const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+    userDetails.email,
+  ]);
+  if (result.rows.length === 0) {
+    return res.status(404).json({
+      status: "not success",
+      message: "user not found",
+    });
+  }
+  const passwordMatch = await bcrypt.compare(
+    userDetails.password,
+    result.rows[0].password,
+  );
+  if (!passwordMatch) {
+    return res.status(401).json({
+      status: "not success",
+      message: "password didnot match",
+    });
+  }
+  const token = jwt.sign(
+    {
+      userId: result.rows[0].id,
+      userEmail: result.rows[0].email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    },
+  );
+  res.status(200).json({
+    status: "success",
+    message: "login succeful",
+    token,
+  });
+};
+export { registeruser, loginUsers };
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,18 +1,21 @@
 import express from "express";
 import {
-    addNewTransaction,
+  addNewTransaction,
   deleteTransaction,
   getAllTransactions,
   getTransactionByID,
   updateTransaction,
 } from "../controllers/transactionController.js";
 import validateTransaction from "../middleware/validateTransaction.js";
+import { loginUsers, registeruser } from "../controllers/authController.js";
 const router = express.Router();
 
 router.get("/transaction", getAllTransactions);
 router.get("/transaction/:id", getTransactionByID);
-router.post("/transaction",validateTransaction,addNewTransaction);
-router.patch("/transaction/:id",updateTransaction)
-router.delete("/transaction/:id",deleteTransaction)
+router.post("/transaction", validateTransaction, addNewTransaction);
+router.patch("/transaction/:id", updateTransaction);
+router.delete("/transaction/:id", deleteTransaction);
+router.post("/auth/register", registeruser);
+router.post("/auth/login", loginUsers);
 
 export default router;
